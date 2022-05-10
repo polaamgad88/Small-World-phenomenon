f6aefc45bc895bd2e9d31a22352ccd1d848ec7ee
kareems3fan <101336843+kareems3fan@users.noreply.github.com>
2022-05-10 21:00:05 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,101 +1,66 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.IO;
-using System.Data;
-namespace Algoo
+
+using System.IO; 
+using System.Data; 
+
+
+namespace Small_World_Phenomenon
 {
     class Program
     {
+
         public class movie
         {
+
             public string Mname;
-            public List<string> actors = new List<string>();
-            int Id;
-        }
-        class queries
-        {
-            public string actor1;
-            public string actor2;
-        }
-        //class adjacentList
-        //{
-        //    LinkedList<Tuple<int, int>>[] adjList;
-        //    public adjacentList(int no_of_actors)
-        //    {
-        //        adjList = new LinkedList<Tuple<int, int>>[no_of_actors];
-        //        for(int i = 0; i < adjList.Length; i++)
-        //        {
-        //            adjList[i] = new LinkedList<Tuple<int, int>>();
-        //        }
-        //    }
-        //    public void addEdgeAtEnd (int startV , int endV , int w )
-        //    {
-        //        adjList[startV].AddLast(new Tuple<int, int>(endV, w));
 
-        //    }
-        //    public int getnoOFactors() 
-        //    {
-        //        return adjList.Length;
-        //    }
-        //    public bool removeEdge(int startV, int endV, int w)
-        //    {
-        //        Tuple<int, int> edge = new Tuple<int, int>(endV, we);
-
-        //        return adjList[startV].Remove(edge);
-        //    }
-        //    public void printAdjList() 
-        //    {
-        //        int index = 0; 
-        //        foreach(LinkedList<Tuple<int,int>>  list in adjList)
-        //        {
-        //            Console.Write("adjList" + index + "->");
-        //            foreach (Tuple<int,int> edge in  list){
-        //                Console.Write(edge.Item1 + "(" + edge.Item2 + ")");
-        //            }
-        //            ++index;
-        //            Console.WriteLine();
-        //        } 
-        //    }
-
-
-        //}
-
-        public static Dictionary<string, List<string>> actors = new Dictionary<string, List<string>>();
-
-        public static class graph {
-        public static void add(string actorname, string value)
-        {
-            if (actors.ContainsKey(actorname)) 
+            public List<string> actors;
+            public movie()  
             {
-                    List<string> list = actors[actorname];
-                    if (list.Contains(value) == false)
-                    {
-                        list.Add(value);
-                    }
-                    //else
-                    //{
-                    //    break;
-                    //}
+
+                actors = new List<string>();
+
             }
-            else
-                {
-                    List<string> list = new List<string>();
-                    list.Add(value);
-                    actors.Add(actorname,list);
-                    //this.Add(key, list);
-                }
+
+
+        }
+
+
+
+
+
+        public static List<movie> all_movies = new List<movie>(); 
+        public static HashSet<string> all_actors = new HashSet<string>();
+        public static Dictionary<string,List<string>> adj = new Dictionary<string, List<string>>();
+
+        public static Dictionary<string, List<string>> Actors = new Dictionary<string, List<string>>();
+        public static Dictionary<string, List<string>> Movies = new Dictionary<string, List<string>>();
+        public static void graph ()
+        {
+            //for (int i = 0; i < all_actors.Count(); i++)
+            //{
+            //    adj.Add(new Dictionary<string, string>()); //adding a map in each index in the adjacency list
                 
+            //}
+            foreach (movie movie in all_movies )
+            {
+                
+
+
+
+
+            }
+
+
+
         }
 
-            public static void print()
-            {
-                Console.WriteLine("actor is " + actors);
-            }
-        }
-        public static List<movie> movies_list = new List<movie>();
+
+
         static void Main(string[] args)
         {
 
@@ -104,38 +69,74 @@ namespace Algoo
             StreamReader sr = new StreamReader(fs);
             string line = null;
             movie m = new movie();
-            // my_movie.movie_name = line_splited[0];
-            //foreach (var word in splitted_words)
-            //{
-            //graph.actors.Add(splitted_words[]);
-            //    //System.Console.WriteLine("word");
 
-            //}
-            // [0] movie name , [1] actor name , [2] , actor name 2 
             while (sr.Peek() != -1)
-            {                
+            {
+
+                movie new_movie = new movie();
+
                 line = sr.ReadLine();
 
-                string[] splitted_words = line.Split('/');
-                m.Mname = splitted_words[0];
-                for (int i = 1; i < splitted_words.Count(); i++)
+                string[] line_splited = line.Split('/');
+
+                new_movie.Mname = line_splited[0];
+                Actors[new_movie.Mname] = new List<string>();
+
+                for (int i = 1; i < line_splited.Count(); i++)
                 {
-                //string v = splitted_words[i + 1];
-                //graph.actors.Add(splitted_words[i],v);
-                    graph.add(splitted_words[i], splitted_words[i + 1]);
-                    m.actors.Add(splitted_words[i]);
 
+                    Actors[new_movie.Mname].Add(line_splited[i]);
+                    if (!Movies.ContainsKey(line_splited[i]))
+                        Movies[line_splited[i]] = new List<string>();
+
+                    Movies[line_splited[i]].Add(new_movie.Mname);
+                    all_actors.Add(line_splited[i]);
+
+                    new_movie.actors.Add(line_splited[i]);
                 }
-            movies_list.Add(m);
 
-            //Console.WriteLine("Hello World!");
+
+                all_movies.Add(new_movie);
+
             }
 
-            //for (int i = 0; i < actors.Count(); i++)
+
+            //for (int i = 0; i < adj.Count(); i++)
             //{
-            //    Console.WriteLine("actor is " + graph.);
+
+            //   foreach (var item in adj[i])
+            //        Console.Write(item);
+            //    Console.WriteLine();
+
             //}
 
+            foreach (string actor in all_actors)
+            {
+                List<string> movies = Movies[actor];
+                HashSet<string> actors = new HashSet<string>();
+                foreach (string movie in movies)
+                {
+                    List<string> acs = Actors[movie];
+                    foreach (string ac in acs)
+                        if (ac != actor)
+                            actors.Add(ac);
+                }
+                adj[actor] = actors.ToList();
+                actors = new HashSet<string>();
+            }
+  foreach(var b in all_actors)
+            {
+                Console.Write(b);
+                Console.Write(">>>");
+
+                foreach (var c in adj[b])
+                    
+
+                    Console.Write(c);
+                Console.WriteLine();
+            }
+
         }
-        }
+
     }
+}
