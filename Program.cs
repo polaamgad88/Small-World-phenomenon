d76b8b6699049276fb79fdd7821de0452b5ace95
Adel Amgad Salama <60940565+polaamgad88@users.noreply.github.com>
2022-05-16 19:23:37 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,50 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System;
+using System.Linq; 
 using System.Diagnostics;
 using System.Threading;
-
 using System.IO;
 using System.Data;
-
-
 namespace Small_World_Phenomenon
 {
     class Program
     {
-
         public class movie
         {
-
             public string Mname;
-
             public List<string> actors;
             public movie()
             {
-
                 actors = new List<string>();
-
             }
-
-
         }
         public class queries
         {
-
             public string actor1, actor2;
-
         }
         public static List<movie> all_movies = new List<movie>();
         public static HashSet<string> all_actors = new HashSet<string>();
         public static Dictionary<string, List<string>> adj = new Dictionary<string, List<string>>();
-
         public static Dictionary<string, List<string>> Actors = new Dictionary<string, List<string>>();
         public static Dictionary<string, List<string>> Movies = new Dictionary<string, List<string>>();
         public static void graph()
         {
-
-
             foreach (string actor in all_actors)
             {
                 List<string> movies = Movies[actor];
@@ -61,43 +46,66 @@ namespace Small_World_Phenomenon
                 adj[actor] = actors.ToList();
                 actors = new HashSet<string>();
             }
-
-
-
         }
         static void BFS(string actor1_str, string actor2_str)
         {
             List<string> path = new List<string>();
 
             Dictionary<string, bool> visited = new Dictionary<string, bool>();
-            int size = all_actors.Count();
             Dictionary<string, string> parent = new Dictionary<string, string>();
             Queue<string> q = new Queue<string>();
+            Dictionary<string, int> strngth = new Dictionary<string, int>();
             q.Enqueue(actor1_str);
             visited[actor1_str] = true;
             parent[actor1_str] = null;
+            strngth.Add(actor1_str, 0);
+            Dictionary<string, int> distance = new Dictionary<string, int>();
+            distance.Add(actor1_str, 0);
             while (q.Count() != 0)
             {
                 string node = q.Dequeue();
+
                 foreach (string v in adj[node])
                 {
                     if (!visited.ContainsKey(v))
                     {
                         visited[v] = false;
+
+                        int x = strength(v, node);
+                        strngth.Add(v, x + strngth[node]);
+                        distance.Add(v, distance[node] + 1);
                     }
                     if (visited[v])
                     {
+                        if (distance[v] > distance[node])
+                        {
+                            int x = strength(v, node);
+                            x += strngth[node];
+                            if (strngth[v] < x)
+                            {
+                                strngth[v] = x;
+                            }
+                        }
                         continue;
+
+
                     }
                     visited[v] = true;
                     parent[v] = node;
                     q.Enqueue(v);
                     if (v == actor2_str)
                     {
+
                         break;
                     }
+
+
                 }
+
             }
+
+
+
             string cur = actor2_str;
             while (cur != null)
             {
@@ -105,24 +113,35 @@ namespace Small_World_Phenomenon
                 cur = parent[cur];
             }
             path.Reverse();
-            //foreach (string v in path)
-            //{
-            //    Console.Write(v + "\t");
-            //}
-            //Console.WriteLine();
             degre_of_seprations(path);
-            Relation_strenght(path);
+            Console.WriteLine(" RoS = "+strngth[actor2_str] );
+            Console.Write("CHAIN OF ACTORS:  =>   ");
+            foreach (var vv in path)
+            {
+                Console.Write(vv + "->");
+            }
+
+            Console.WriteLine("");
+            Console.Write("CHAIN OF MOVIES:  =>   ");
             chain_of_movies(path);
             
+            
         }
+        public static int strength(string s1, string s2)
+        {
+            List<string> s = new List<string>();
+            s = Movies[s1].Intersect(Movies[s2]).ToList();
+
+            return s.Count();
+        }
+
         public static void degre_of_seprations(List<string> s)
         {
             int x = s.Count();
             x = x - 1;
-            Console.Write("\t" + x+ "\t");
+            Console.Write("DoS =" + x+", ");
         }
         public static void Relation_strenght(List<string> path)
-
         {
             int sum = 0;
             List<string> ac = new List<string>();
@@ -131,7 +150,7 @@ namespace Small_World_Phenomenon
             {
                 ac = Movies[path[i]].Intersect(Movies[path[i + 1]]).ToList();
                 sum += ac.Count();
-            }
+            }  
             Console.Write(sum+ "\t");
         }
         public static void chain_of_movies(List<string> path)
@@ -139,21 +158,41 @@ namespace Small_World_Phenomenon
             //string[] mov = new string[path.Count()];
             Dictionary<int, List<string>> chain = new Dictionary<int, List<string>>();
             for (int i = 0; i < path.Count() - 1; i++)
-            {
-
+            {               
                 chain[i] = (Movies[path[i]].Intersect(Movies[path[i + 1]]).ToList());
-
-
             }
             for (int i = 0; i < chain.Count(); i++)
             {
+                int s = chain[i].Count();
                 foreach (string v in chain[i])
-                    Console.Write(v + " ");
+                {
+
+                    if (s > 1)
+                    {
+
+                        //Console.Write(v+" or  " );
+                        Console.Write(v + " or  ");
+                        
+                    }
+                    else if (i < chain.Count() - 1)
+                    {
+                        Console.Write(v  + " =>"+ "  ");
+
+                    }
+                    else
+                    {
+                        
+
+                        Console.Write(" " + v);
+                    }
+                    s--;
+                }
             }
             Console.WriteLine();
-
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
         }
-
         //public static void chain_of_movies (List<string> s)
         //{
         // could be optimized chain of movie ;;;;;;;;;;;
@@ -171,33 +210,23 @@ namespace Small_World_Phenomenon
         public static void load_queries()
         {
             // the following part is for loading queries
-
-            FileStream queries_file = new FileStream("queries200.txt", FileMode.Open, FileAccess.Read);
-
+            FileStream queries_file = new FileStream("queries26.txt", FileMode.Open, FileAccess.Read);
             StreamReader queries_SR = new StreamReader(queries_file);
-
             string actors = null;
-            Console.WriteLine("Query" + "\t" + "Deg." + "\t" + "Rel." + "\t" + "Chain");
-
+            //Console.WriteLine("Query" + "\t" + "Deg." + "\t" + "Rel." + "\t" + "Chain");
             while (queries_SR.Peek() != -1)
             {
-
                 queries my_querie = new queries();
-
                 actors = queries_SR.ReadLine();
-
                 string[] actors_splited = actors.Split('/');
-
                 my_querie.actor1 = actors_splited[0];
-
                 my_querie.actor2 = actors_splited[1];
-                Console.Write(my_querie.actor1 + "/" + my_querie.actor2);
-                BFS(my_querie.actor1, my_querie.actor2);
-                
-            }
-            
+               //if sample test
+               //Console.Write(my_querie.actor1 + "/" + my_querie.actor2);
+               Console.WriteLine(my_querie.actor1 + "/" + my_querie.actor2);
+                BFS(my_querie.actor1, my_querie.actor2);            
+            }   
             queries_SR.Close();
-
         }
         //public static void Relation_strenght(queries my, string a, string b)
         //{
@@ -213,38 +242,39 @@ namespace Small_World_Phenomenon
         //        //List<string> common = Movies[a].Intersect(Movies[b]).ToList();
         //        // Console.WriteLine(common.Count);
         //    }
-          
-            //for (int i = 0; i < Movies[a].Count(); i++)
-            //{
-            //    for(int j = 0; j < Movies[b].Count(); i++)
-            //    {
-            //        if (Movies[a][i] == Movies[b][j])
-            //        {
-            //            counter++;
-            //            common_films.Add(a);
-            //        }
-            //        else
-            //        {
-            //            continue;
-            //        }
-            //    }
-            //}
 
-            //for (int i = 0; i < all_movies.Count(); i++)
-            //{
+        //for (int i = 0; i < Movies[a].Count(); i++)
+        //{
+        //    for(int j = 0; j < Movies[b].Count(); i++)
+        //    {
+        //        if (Movies[a][i] == Movies[b][j])
+        //        {
+        //            counter++;
+        //            common_films.Add(a);
+        //        }
+        //        else
+        //        {
+        //            continue;
+        //        }
+        //    }
+        //}
 
-            // common_films = (List<string>)adj[a].Intersect(adj[b]) ;
+        //for (int i = 0; i < all_movies.Count(); i++)
+        //{
 
-            //}
-            // Console.WriteLine("Relation strenght ---->> " + common_films.Count());
-       // }
+        // common_films = (List<string>)adj[a].Intersect(adj[b]) ;
+
+        //}
+        // Console.WriteLine("Relation strenght ---->> " + common_films.Count());
+        // }
+
         public static void load_movies()
         {
-            FileStream fs = new FileStream("Movies122806.txt", FileMode.Open, FileAccess.Read);
+
+            FileStream fs = new FileStream("Movies14129.txt", FileMode.Open, FileAccess.Read);
             StreamReader sr = new StreamReader(fs);
             string line = null;
-            movie m = new movie();
-
+           // movie m = new movie();
             while (sr.Peek() != -1)
             {
 
@@ -279,19 +309,18 @@ namespace Small_World_Phenomenon
         }
         static void Main(string[] args)
         {
-
+            DateTime start = DateTime.Now;
+            
             load_movies();
             load_queries();
-            Stopwatch stopwatch = new Stopwatch();
 
-            stopwatch.Start();
+
+
             Thread.Sleep(5000);
-            stopwatch.Stop();
+            DateTime end = DateTime.Now;
+            TimeSpan Tss = (end - start);
+            Console.WriteLine("time =  " + Tss.TotalSeconds);
 
-            TimeSpan ts = stopwatch.Elapsed;
-
-            Console.WriteLine("Elapsed Time is {0:00}:{1:00}:{2:00}.{3}",
-                            ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds);
         }
 
     }
